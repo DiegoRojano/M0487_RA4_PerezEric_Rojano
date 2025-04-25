6e3467b9f743def5255bbe6091bb7b264d41025e
infor <drojano@ies-eugeni.cat>
2025-04-25 16:27:15 +0200

    Tercer Commit

--- test_crear_bbdd.py
+++ test_crear_bbdd.py
@@ -1,11 +1,19 @@
 import unittest
 import sqlite3
-from crear_bbdd import afegir_grup, mostrar_grups, eliminar_grup, actualitzar_grup, crear_base_dades
+from unittest.mock import patch
+from crear_bbdd import (
+    afegir_grup,
+    consultar_grup_per_nom,
+    mostrar_grups,
+    eliminar_grup,
+    actualitzar_grup,
+    intro_dades,
+    crear_base_dades
+)
 
 class TestGestorMusica(unittest.TestCase):
 
     def setUp(self):
-
         crear_base_dades()
         conn = sqlite3.connect("grups.db")
         cursor = conn.cursor()
@@ -34,6 +42,19 @@ class TestGestorMusica(unittest.TestCase):
         afegir_grup("Buhos", 2012, "rock", 6)
         grups = mostrar_grups()
         self.assertTrue(len(grups) > 0)
+
+    def test_consultar_grup_per_nom(self):
+        afegir_grup("Els Pets", 1985, "rock", 3)
+        resultat = consultar_grup_per_nom("Els Pets")
+        self.assertEqual(resultat, ("Els Pets", 1985, "rock", 3))
+
+    @patch("builtins.input", side_effect=["Manel", "2008", "folk", "4"])
+    def test_intro_dades_nou_grup(self, mock_input):
+        
+        with patch("builtins.print") as mock_print:
+            dades = intro_dades()
+            self.assertIsNone(dades)
+            
 
 if __name__ == '__main__':
     unittest.main()
